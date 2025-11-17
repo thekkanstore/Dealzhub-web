b3062db3ceee19dcd865e224468c9ace226e871d
Ajay M V <ajaymvaju17@gmail.com>
2025-11-17 22:24:50 +0530

    removed the router

--- src/main.tsx
+++ src/main.tsx
@@ -7,8 +7,6 @@ import { HashRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <HashRouter>
-      <App />
-    </HashRouter>
+    <App />
   </React.StrictMode>,
 );
